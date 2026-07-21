98e4d0aeb7f48c8d3a97360ef6191d9bac02ff8b
ralph-abejuela <abejuela.ralph.balatucan@gmail.com>
2026-07-21 13:17:54 +0800

    Added shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/ejobtrack-frontend/",
-  plugins: [tanstackRouter(), react()],
+  plugins: [tanstackRouter(), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
